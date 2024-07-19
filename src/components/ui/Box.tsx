b10bb96d9ab05308070ec77e64169431c89cc359
Alireza valizadeh <alirezavalizade0@gmail.com>
2024-07-19 09:43:38 +0200

    update Box

--- src/components/ui/Box.tsx
+++ src/components/ui/Box.tsx
@@ -1,5 +1,13 @@
 import React, { forwardRef, ReactElement, ReactNode, ElementType, Ref } from "react";
 
+// This component is a versatile Box container. (Inspired from Chakra-ui)
+// The Box component is designed to allow for clean and extendable code.
+// It can be used as a wrapper for any HTML element or React component,
+// making it easy to apply consistent styling and structure across the application.
+// - Clean code: Provides a clean and consistent way to define components with styling and other props.
+// - Extendable: Easily extendable to include additional functionality like flex or grid helpers.
+// - Reusable: Promotes reusability of code by abstracting common patterns.
+
 export interface BoxProps {
   children?: ReactNode;
   as?: ElementType | ReactElement;
